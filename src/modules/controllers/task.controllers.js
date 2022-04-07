4c05c203190ea29372d6f197e6d134c4082082a6
ValentinBesputnev <besputnev.exceedteam@gmail.com>
2022-04-07 10:24:09 +0300

    created post route

--- src/modules/controllers/task.controllers.js
+++ src/modules/controllers/task.controllers.js
@@ -9,3 +9,16 @@ module.exports.getAllExpenses = (req, res) => {
     res.status(500).send({ message: "Internal server error" });
   }
 };
+
+module.exports.createNewExpense = (req, res) => {
+  try {
+    const newExpense = new Expenses(req.body);
+    newExpense.save().then(() => {
+      Expenses.find().then((result) => {
+        res.send({ data: result });
+      });
+    });
+  } catch (e) {
+    res.status(500).send({ message: "Internal server error" });
+  }
+};
